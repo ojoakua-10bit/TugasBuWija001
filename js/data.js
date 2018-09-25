0bf2b9e224f3d5bd8587f40a7d56477aa3825b9e
Aditya Pramana <mirror.ojoakua@gmail.com>
2018-09-25 23:32:09 +0700

    Minor additions

---
+++ js/data.js
@@ -0,0 +1,9 @@
+/*
+ * js/data.js
+ * This is the declaration of vertices data
+ */
+
+let points = [
+    vec2(-0.923, 0.533),
+    vec2(-0.923, -0.566)
+];
---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*
+ * js/main.js
+ * This is the main logic of the WebGL program.
+ */
+
+let gl;
+
+window.onload = function () {
+    let ctx = document.getElementById("main-canvas");
+    gl = WebGLUtils.setupWebGL(ctx);
+    if (!gl) alert("Your browser doesn't support canvas!");
+
+    // Configure WebGL
+    gl.viewport(0, 0, ctx.width, ctx.height);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Load shaders and initialize attribute
+    let program01 = initShaders(gl, "vShader01", "fShader01");
+    gl.useProgram(program01);
+
+    // Load the data into GPU
+    let buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffers
+    var vLoc1 = gl.getAttribLocation(program01, "vLoc1");
+    gl.vertexAttribPointer(vLoc1, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vLoc1);
+
+    // Render at least 60 FPS
+    setTimeout(function render() {
+        requestAnimFrame(render);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.LINE_LOOP, 0, points.length);
+    }, 16.6667)
+};
